00a9d282ab75b26f4cd1207d0fe084df3babf38d
Vittorio Larovere <vittorio.larovere@facile.it>
2020-12-09 09:48:30 +0100

    Esercizio1

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,2 +1,10 @@
-// istanbul ignore file
-console.log("ok");
+type isString<A> = A extends string ? true : false;
+
+type A = isString<"hello">;
+type B = isString<number>
+
+type C = "hello" extends string ? true : false;
+
+type D<A> = A extends string ? {foo: A}: never;
+
+type E = D<number>;
